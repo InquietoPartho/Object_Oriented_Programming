0ec0c6959205f179f5835958ef7111926a4b693d
Pijush Kanti Roy Partho <129577070+InquietoPartho@users.noreply.github.com>
2024-01-10 17:14:09 +0600

    Adding new files

---
+++ pointer_to_objects.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class test{
+        public:
+            void show(){
+                cout<<"Hello World! This is Pijush Kanti Roy Partho"<<endl;
+            }
+};
+int main(){
+    test *ptr;
+    ptr = new test;
+    ptr->show();
+}
